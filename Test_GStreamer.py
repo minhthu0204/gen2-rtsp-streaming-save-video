a49b3bff57a1cd0c447a16cfcb5a935efaab912c
Phulinh <phulinhnguyen3@gmail.com>
2025-01-07 12:16:28 +0700

    11th

--- Test_GStreamer.py
+++ Test_GStreamer.py
@@ -37,7 +37,7 @@ class UdpStream:
         self.pipeline = Gst.parse_launch(
             'appsrc name=source is-live=true block=true format=GST_FORMAT_TIME ! '
             'h265parse ! tee name=t '
-            't. ! queue ! rtph265pay pt=96 ! udpsink host={} port={}'
+            't. ! queue ! rtph265pay pt=96 ! udpsink host={} port={} '
             't. ! queue ! mp4mux ! fakesink'.format(self.host, self.port)
         )
         appsrc = self.pipeline.get_by_name('source')
